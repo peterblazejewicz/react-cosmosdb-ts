db1819dd318d564c8b0d5e9df4bb4298504b774e
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2017-09-17 15:18:19 +0200

    App to components, css

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,13 +4,9 @@ import React from 'react';
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+      <div>
+        <h1>Heroes</h1>
+        <div className="header-bar" />
       </div>
     );
   }
